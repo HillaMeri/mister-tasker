7bc81769f32a4a1615daee89b58dfbf01b6b69c7
hilla <hilla7070@gmail.com>
2020-10-04 17:48:42 +0300

    asd

---
+++ backend/public/precache-manifest.ba5dbc72abf7aaef17873efbf3c2633a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104cb1e4490feb42fd7de7e1251f9389",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a67098a1d23e7abc4c4e",
+    "url": "/static/css/main.c3a26e89.chunk.css"
+  },
+  {
+    "revision": "0ae14767369acd1e8de0",
+    "url": "/static/js/2.042a47a8.chunk.js"
+  },
+  {
+    "revision": "85a08104991a260e6b3c376fd8924b35",
+    "url": "/static/js/2.042a47a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a67098a1d23e7abc4c4e",
+    "url": "/static/js/main.94a9d9fe.chunk.js"
+  },
+  {
+    "revision": "c9f1875f7e246a67df7e",
+    "url": "/static/js/runtime-main.cb82405f.js"
+  },
+  {
+    "revision": "7609e38d314346f3ec8bdf891b974d9f",
+    "url": "/static/media/7.7609e38d.jpg"
+  },
+  {
+    "revision": "10c6818802ed96f9d6976633ae148c37",
+    "url": "/static/media/Anton-Regular.10c68188.ttf"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "c5a505e73a3faf83909861000ebe4bb7",
+    "url": "/static/media/Hanalei-Regular.c5a505e7.ttf"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "62ae6cede5a638fee57431d1d7af0eab",
+    "url": "/static/media/Rancho-Regular.62ae6ced.ttf"
+  }
+]);
